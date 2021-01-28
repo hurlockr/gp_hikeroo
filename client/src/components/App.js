729de997e1adbb518ad500ccf3cf75be31060954
Viji <71524056+viji9898@users.noreply.github.com>
2021-01-28 14:17:01 -0500

    Add a showpage for each trail - dynamic link with :id

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ import "../assets/scss/main.scss"
 import RegistrationForm from "./registration/RegistrationForm"
 import SignInForm from "./authentication/SignInForm"
 import TopBar from "./layout/TopBar"
-
+import TrailShowPage from "./TrailShowPage"
 import TrailsPage from "./TrailsPage"
 
 const App = (props) => {
@@ -31,6 +31,7 @@ const App = (props) => {
         <Route exact path="/trails" component={TrailsPage} />
         <Route exact path="/users/new" component={RegistrationForm} />
         <Route exact path="/user-sessions/new" component={SignInForm} />
+        <Route exact path="/trails/:id" component={TrailShowPage} />
       </Switch>
     </Router>
   )
