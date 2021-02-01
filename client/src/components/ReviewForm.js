36ee35e14fea95ac9bf35b6d9f7c388039a2ed39
Ross Hurlock <jrlhurlock@gmail.com>
2021-02-01 16:25:01 -0500

    adding review form to new branch

---
+++ client/src/components/ReviewForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+
+const ReviewForm = ({ postReview }) => {
+  const [newReview, setNewReview] = useState({
+    comment: "",
+    rating: "",
+  })
+
+  const handleInputChange = (event) => {
+    setNewReview({
+      ...newReview,
+      [event.currentTarget.name]: event.currentTarget.value,
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    postReview(newReview)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          comment:
+          <input
+            type="text"
+            name="comment"
+            onChange={handleInputChange}
+            value={newReview.comment}
+          />
+        </label>
+
+        <label>
+          rating:
+          <input type="text" name="rating" onChange={handleInputChange} value={newReview.rating} />
+        </label>
+        <div>
+          <input className="button" type="submit" value="submit" />
+        </div>
+      </form>
+    </div>
+  )
+}
+export default ReviewForm
