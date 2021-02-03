177ce5d87c22eca20ac99e4b68ce8b33a9cca082
mshkolnik22 <75180384+mshkolnik22@users.noreply.github.com>
2021-02-03 13:38:58 -0500

    updated import statement in TrailsPage.js (#10)
    Co-authored-by: Maria Shkolnik <mariashkolnik@marias-air.lan>

--- client/src/components/TrailsPage.js
+++ client/src/components/TrailsPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import TrailForm from "./trailForm"
+import TrailForm from "./TrailForm"
 import ErrorList from "./ErrorList"
 import translateServerErrors from "../services/translateServerErrors"
 
